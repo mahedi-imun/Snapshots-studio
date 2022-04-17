fae8a2346dab7ccbcc0b76b42163d458bd9a958d
mahedi <mahedianik4@gmail.com>
2022-04-17 22:06:02 +0600

    add footer style

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -9,6 +9,10 @@ const About = () => {
                     <h4>I am mahedi a  baby web developer</h4>
                     <h6>I already learned the  HTML, CSS,JAVASCRIPT , REACT , REACT ROUTER , FIREBASE AUTHENTICATION.</h6>
                     <p>I can build any  website. I can even teach my grandma how to make  website. My goal is do job professional software company and learn advanced topics.</p>
+                    <p>From January 1 - I am still working 12/14 hours a day.
+                        It may not be too late to reach the peak of dreams. 
+                    </p>
+                        
                 </div>
                 <div className='w-50 d-flex justify-content-center align-items-center' >
                     <img className='w-100' src={aboutImg1} alt="" />
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div >
-            <p className='text-center'> &copy; Copyright Snapshots {year} All rights reserved.</p>
+        <div style={{height:'200px',color:'#fdee17',backgroundColor:'#212529'}} className=' border-top '>
+            <p className='d-flex justify-content-center h-100 align-items-center'> &copy; Copyright Snapshots {year} All rights reserved.</p>
         </div>
     );
 };
