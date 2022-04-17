19d78e49b682c03de6fea0c19bb26f90161a7176
mahedi <mahedianik4@gmail.com>
2022-04-17 22:19:37 +0600

    add 404 page style

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import image from '../../../images/404 page/Funny-404-Pages.gif'
 const NotFound = () => {
     return (
-        <div>
-            <p>page not found </p>
+        <div style={{
+            color: 'red', backgroundImage: `url(${image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundColor: '#000000',
+            height: '100vh'
+        }}>
         </div>
     );
 };
